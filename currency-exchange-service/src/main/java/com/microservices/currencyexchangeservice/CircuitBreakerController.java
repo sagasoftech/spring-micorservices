2106e54abebb5da637d6245015c10f3e6fa1bffd
Sagar Dighe <sagasoftech@gmail.com>
2023-05-02 03:27:26 -0500

    BulkHead Set maximum number of concurrent call to the srvice

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -20,7 +21,8 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	//@Retry(name="sample-api", fallbackMethod = "hardcodedMethod")
 	//@CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedMethod")
-	@RateLimiter(name="sample-api")
+	//@RateLimiter(name="sample-api")
+	@Bulkhead(name="sample-api")
 	public String sampleApi() {
 		logger.info("Sample API call received");
 		//ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-service", String.class);
