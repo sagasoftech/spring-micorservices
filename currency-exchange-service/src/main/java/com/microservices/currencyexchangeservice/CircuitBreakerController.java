cb4c6310ab6a99523bbfd7a90e5b3c261c772c50
Sagar Dighe <sagasoftech@gmail.com>
2023-05-02 03:23:23 -0500

    Add RateLimiter to limit number of calls per seconds

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -18,11 +19,13 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/sample-api")
 	//@Retry(name="sample-api", fallbackMethod = "hardcodedMethod")
-	@CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedMethod")
+	//@CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedMethod")
+	@RateLimiter(name="sample-api")
 	public String sampleApi() {
 		logger.info("Sample API call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-service", String.class);
-		return forEntity.getBody();
+		//ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-service", String.class);
+		//return forEntity.getBody();
+		return "sample-api";
 	}
 	
 	public String hardcodedMethod(Exception ex) {
