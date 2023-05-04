674c759fa01b9d834f104ed16fa2737644cdb0a6
Sagar Dighe <sagasoftech@gmail.com>
2023-05-04 01:25:28 -0500

    Additional logging for Currency Exchange service

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
 	public CurrencyExchage retriveExcangeValue(
 			@PathVariable String from, 
 			@PathVariable String to) {
+		
+		logger.info("retriveExcangeValue called with {} and {}", from, to);
 		
 		CurrencyExchage currencyExchage = repository.findByFromAndTo(from, to);
 		
